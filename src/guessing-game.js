4262afd61eafb7629d316cbab09ed8be5c7430f5
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-08 10:34:49 +0300

    feat: add function code to quessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,17 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.low = 0;
+        this.high = 0;
+        this.result = 0;
     }
 
-    guess() {
+    setRange = (min, max) => this.high = max;
 
-    }
+    guess = () => this.result = Math.ceil((this.low + this.high)/2);
 
-    lower() {
+    lower = () => this.high = this.result;
 
-    }
-
-    greater() {
-
-    }
+    greater = () => this.low = this.result;
 }
 
 module.exports = GuessingGame;
